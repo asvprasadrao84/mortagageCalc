271c25704230f641f76998ab44fdaf22d33e5d95
Prasad84 <asvprasadrao@gmail.com>
2024-11-03 10:07:40 +0100

    Updated default to App

---
+++ App.jsx
@@ -0,0 +1,30 @@
+// src/App.jsx
+import React from 'react';
+import Calculator from './components/Calculator';
+import Layout from './components/Layout';
+
+export default function App() {
+  const [calculationResults, setCalculationResults] = React.useState(null);
+  
+  const handleCalculation = (results) => {
+    setCalculationResults(results);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Layout>
+        <div className="space-y-8">
+          <Calculator onCalculate={handleCalculation} />
+          
+          {calculationResults && (
+            <>
+              <LoanSummary results={calculationResults} />
+              <AmortizationTable results={calculationResults} />
+              <InvestmentGrowth />
+            </>
+          )}
+        </div>
+      </Layout>
+    </div>
+  );
+}
